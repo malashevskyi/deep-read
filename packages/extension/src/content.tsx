9354dc1b15d99d375a18e4f99b28ae05ec635f0d
Vitalii Malashevskyi <shevsky1488@gmail.com>
2025-09-27 13:00:25 +0300

    refactor: extract style injection into util

--- packages/extension/src/content.tsx
+++ packages/extension/src/content.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Sidebar } from "./components/Layout/Sidebar";
 import sidebarCssString from "./index.css?inline";
 import hostPageStyles from "./hostPageStyles.css?inline";
+import { injectStyles } from "./utils/injectStyles";
 
 console.log("[DeepRead] Content script loaded!");
 
@@ -10,17 +11,7 @@ let rootElement: HTMLElement | null = null;
 let reactRoot: ReactDOM.Root | null = null;
 const SIDEBAR_OPEN_BODY_CLASS = "deepread-sidebar-open";
 
-function injectHostPageStyles() {
-  const styleElementId = "deepread-host-styles";
-  if (document.getElementById(styleElementId)) return;
-
-  const styleElement = document.createElement("style");
-  styleElement.id = styleElementId;
-  styleElement.innerHTML = hostPageStyles;
-  document.head.appendChild(styleElement);
-}
-
-injectHostPageStyles();
+injectStyles(hostPageStyles, "deepread-host-styles");
 
 function mountSidebar(selectedText: string) {
   if (rootElement) unmountSidebar();
