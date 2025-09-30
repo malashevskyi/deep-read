b945a4ffb42bc97fa6c2570dc381a6cef65d13ae
Vitalii Malashevskyi <shevsky1488@gmail.com>
2025-09-30 15:10:12 +0300

    chore: add react compiler babel plugin

--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -9,5 +9,14 @@ export default defineConfig({
   // Ensures that asset paths are relative, which is required for
   // `chrome.runtime.getURL` to work correctly in content scripts.
   base: "",
-  plugins: [react(), crx({ manifest }), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    ,
+    crx({ manifest }),
+    tailwindcss(),
+  ],
 });
